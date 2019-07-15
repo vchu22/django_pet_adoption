dc1e586cdd2b88346f4a9305adc441ecffbda3ad
Vanessa Chu <vchu22@users.noreply.github.com>
2019-07-15 09:30:04 -0400

    set up url patterns

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(req):
+    return HttpResponse('<p>home view</p>')
+
+def pet_detail(req, id):
+    return HttpResponse('<p>pet_detail view with id {}</p>'.format(id))
